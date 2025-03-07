cf88d119f5e1357a0bf9c89a2635c5187a49bdb4
Spen-Fars <sdfarley@lclark.edu>
2025-03-07 23:42:15 +0000

    star test 4

--- carousel.php
+++ carousel.php
@@ -9,11 +9,11 @@
 
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+        rel="stylesheet" crossorigin="anonymous"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3">
 
-   rel="stylesheet" crossorigin="anonymous"
-   integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3">
-   <link href="css/style.css" rel="stylesheet">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
     </head>
     <body>
@@ -30,6 +30,7 @@
         <!-- Page content-->
         <div class="container mt-5">
 
+        <div>hi</div>
         <span class="rating"> 
             <i class="fa-solid fa-star"></i> 
             <i class="fa-solid fa-star"></i> 
@@ -37,8 +38,6 @@
             <i class="fa-solid fa-star"></i> 
             <i class="fa-regular fa-star"></i> 
         </span>
-
-
 
         </div>
         <!-- Bootstrap core JS-->
